700e4c80494b3afa68e7d3137565fb21cbbde3f6
AlexDr81 <ubil432@gmail.com>
2020-02-08 19:37:37 +0500

    Final

--- hw04-classloader/src/main/java/ru/otus/Demo.java
+++ hw04-classloader/src/main/java/ru/otus/Demo.java
@@ -3,6 +3,7 @@ package ru.otus;
 public class Demo {
     public static void main(String[] args) {
         TestLoggingInterface myClass = IoC.createMyClass(new TestLogging());
+
         myClass.calculation(6);
     }
 }
